836dbf2e48cc2da65570f2936f65bb8e03220090
mertdorukkalkan <mertdorukkalkan@gmail.com>
2023-05-06 14:46:09 +0300

    EntityFramework Generic Repository oluşturuldu

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Domain;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<TEntity>
+        where TEntity : BaseEntity, new()
+    {
+        Task AddAsync(TEntity input);
+        Task UpdateAsync(TEntity input);
+        Task DeleteAsync(TEntity input);
+        Task DeleteByIdAsync(int id);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<ICollection<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate = null);
+        Task<TEntity> GetWithIncludeAsync(Expression<Func<TEntity, bool>> predicate,
+            params Expression<Func<TEntity, object>>[] includes);
+        Task<ICollection<TEntity>> GetListWithIncludeAsync(Expression<Func<TEntity, bool>> predicate = null,
+            params Expression<Func<TEntity, object>>[] includes);
+    }
+}
